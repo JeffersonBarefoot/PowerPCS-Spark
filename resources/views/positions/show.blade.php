29b4fab29f91e21fe1265a0eb60080a503d03baf
JeffersonBarefoot <42776676+JeffersonBarefoot@users.noreply.github.com>
2021-02-26 09:48:49 -0500

    Edit Position Description.  Known problem...can edit once, but can't edit a 2nd time.  Added to issues list.

--- resources/views/positions/show.blade.php
+++ resources/views/positions/show.blade.php
@@ -183,8 +183,15 @@
   <div class="row">
       <!-- <div class="col-sm-8 offset-sm-0"> -->
       <div class="col-md-12">
-          <h1 class="display-5">&nbsp;&nbsp;&nbsp;{{$position->descr}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small>{{$position->company}} / {{$position->posno}}</small></h1>
-
+          <h1 class="display-5">
+            &nbsp;&nbsp;&nbsp;
+            @if ($readonly=='readonly')
+              {{$position->descr}}
+            @else
+              <input type="text" class="form-control" name="descr" value="{{$position->descr}}">
+            @endif
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <small>{{$position->company}} / {{$position->posno}}</small></h1>
           <a href={{route('positions.show',$position->id)}}?editmode=switch>{{Session::get('editModeButtonText')}} test</a><br>
           <a href={{route('positions.create')}}>Add New Position </a><br>
           <a href={{ route('verifydestroy') }}?positiontodelete={{$position->id}}>Delete This Position </a><br>
